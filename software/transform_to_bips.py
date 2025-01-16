17270e0e2cdc7e07be68ae54eca6ca8e654fc6ed
Kristen Schneider <kristen.schneider@tempus.com>
2025-01-16 14:52:14 -0600

    adding fucntion to read header fields from the sql output

--- software/transform_to_bips.py
+++ software/transform_to_bips.py
@@ -1,6 +1,48 @@
+import argparse
+import pandas as pd
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', type=str, required=True)
+    parser.add_argument('-o', '--output', type=str, required=True)
+    parser.add_argument('-f', '--fields', type=str, required=False)
+    return parser.parse_args()
+
 def main():
-    x = 'test'
+    x = 'hello world'
     print(x)
+
+    # get arguments
+    args = get_args()
+    sql_input = args.input
+    bips_output = args.output
+    sql_fields = args.fields
+
+    # get fields from the sql query
+    sql_file_fields = get_bioinfdb_query_fields(sql_input, ',')
+
+    # fill in whichever fields are not part of the file input
+    # use user input for this
+
+    # # write out to a bips_input file
+    # o = open(bips_output, 'w')
+    # o.write()
+
+    
+
+def get_bioinfdb_query_fields(bioinf_db_csv, delim):
+    '''
+    Read in a SQL query output file and return the fields included in the header.
+    @param input bioinf_db_csv: SQL query output
+    @param input delim: file delimeter
+    @param return field_list: list of fields from SQL query
+    '''
+    f = open(bioinf_db_csv, 'r')
+    header = f.readline()
+    field_list = header.strip().split(delim)
+    f.close()
+    return field_list
+
 
 if __name__ == '__main__':
     main()
