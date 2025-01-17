2082ac6a1cc000411e9f4225d308e18ca22c83aa
Kristen Schneider <kristen.schneider@tempus.com>
2025-01-16 20:07:32 -0600

    simple bips bash file

---
+++ software/bips_run.sh
@@ -0,0 +1,5 @@
+input_bips_csv='/Users/kristen.schneider/Research/unattached_tickets/BFXA-5507_bips_run/data/bquxjob_24ab7304_1947033446c-bips_input.csv'
+
+bips $input_bips_csv \ 
+    --log-dir ./ \
+    --transform-override-csv /Users/kristen.schneider/Research/unattached_tickets/BFXA-5507_bips_run/data/transform_override_rnaonco_v140_val.csv
