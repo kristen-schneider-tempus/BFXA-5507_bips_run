8ee390a79bff326f694872f17ca8900e36ce3734
Kristen Schneider <kristen.schneider@tempus.com>
2025-01-28 12:12:36 -0600

    generalizing scripts for new input

--- software/bips_run.sh
+++ software/bips_run.sh
@@ -1,9 +1,10 @@
 # TODO step 1: run the bioinf-db to bips input script once it's more robust
 
 # step 2: run bips
-input_bips_csv='/Users/kristen.schneider/Research/unattached_tickets/BFXA-5507_bips_run/data/bquxjob_24ab7304_1947033446c-bips_input_manual.csv'
+input_bips_csv=$1
+transform_override_csv=$2
 
-bips $input_bips_csv \ 
-    --transform-override-csv /Users/kristen.schneider/Research/unattached_tickets/BFXA-5507_bips_run/data/transform_override_rnaonco_v140_val.csv
+bips $input_bips_csv
+    --transform-override-csv $transform_override_csv
+    --env staging
     # --log_dir /Users/kristen.schneider/Research/unattached_tickets/BFXA-5507_bips_run/results/log
-    # --env staging
