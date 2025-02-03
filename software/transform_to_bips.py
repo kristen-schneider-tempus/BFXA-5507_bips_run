ad2a75b089bf0d8ef5222ff679dc4c8f37d46d36
Kristen Schneider <kristen.schneider@tempus.com>
2025-02-03 12:08:37 -0600

    added functions to trasnform to a bips input file in correct order

--- software/transform_to_bips.py
+++ software/transform_to_bips.py
@@ -7,33 +7,26 @@ BIPS_FIELDS=['orderhub_id',
              'assay',
              'analyte',
              'match_type',
-             'intent',
-             'control_sample_type']
+             'intent']
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input', type=str,
-                        required=True, help='Input SQL query file')
+                        required=True, help='GBQ output')
     parser.add_argument('-o', '--output', type=str,
-                        required=True, help='file to write out new bips query')
-    # parser.add_argument('-n', '--num_records', type=int,
-    #                     required=True, help='number of records to write out')
-    # parser.add_argument('-b', '--orderhub_id', type=str, required=False,
-    #                     default='', help='orderhub ID')
-    # parser.add_argument('-f', '--gcs_tumor_fastq_url', type=str, required=False,
-    #                     default='', help='fastq url from gc')
-    # parser.add_argument('-c', '--cancer_type', type=str, required=False,
-    #                     default='Tumor of Unknown Origin', help='cancer type')
-    # parser.add_argument('-a', '--assay', type=str, required=False,
-    #                     default='', help='assay type')
-    # parser.add_argument('-l', '--analyte', type=str, required=False,
-    #                     default='rna', help='analyte type')
-    # parser.add_argument('-m', '--match_type', type=str, required=False,
-    #                     default='tumor_only', help='tumor-normal match type')
-    # parser.add_argument('-t', '--intent', type=str, required=False,
-    #                     default='rad', help='intent')
-    # parser.add_argument('-s', '--control_sample_type', type=str, required=False, 
-    #                     default='', help='control sample type')
+                        required=True, help='bips input format')
+    parser.add_argument('-l', '--location', type=str,
+                        required=True, help='location of the fastq files (e.g. prd, beta, etc.)')
+    parser.add_argument('-c', '--cancer_type', type=str,
+                        required=True, help='cancer type (ex. "Adenoid Cystic Carcinoma")')
+    parser.add_argument('-a', '--assay', type=str,
+                        required=True, help='assay type (ex. "RNA-onco.v1, RS.v2")')
+    parser.add_argument('-n', '--analyte', type=str,
+                        required=True, help='analyte type (ex. "rna")')
+    parser.add_argument('-m', '--match_type', type=str,
+                        required=True, help='match type (ex. "tumorOnly")')
+    parser.add_argument('-t', '--intent', type=str,
+                        required=True, help='intent (ex. "rad")')
 
     return parser.parse_args()
 
@@ -41,142 +34,91 @@ def main():
 
     # get required arguments
     args = get_args()
-    # REQUIRED
     sql_output_file = args.input
     bips_input_file = args.output
-    num_records = args.num_records
+    location = args.location
+    cancer_type = args.cancer_type
+    assay = args.assay
+    analyte = args.analyte
+    match_type = args.match_type
+    intent = args.intent
 
-    # SUPPLEMENT
-    user_input = {bf:'' for bf in BIPS_FIELDS}
-    user_input['ohid'] = args.orderhub_id
-    user_input['fq'] = args.gcs_tumor_fastq_url
-    user_input['cncr'] = args.cancer_type
-    user_input['as'] = args.assay
-    user_input['an'] = args.analyte
-    user_input['mt'] = args.match_type
-    user_input['int'] = args.intent
-    user_input['cst'] = args.control_sample_type
-
-    # get fields from the sql query
-    input_file_field_dict = get_bioinfdb_query_data(sql_output_file,
-                                                     ',') 
-
-    # fill output dict with input data + added fields
-    bips_output_dict = fill_bips_data(input_file_field_dict,
-                                      user_input,
-                                      num_records)
+    # read the sql output file
+    sql_df = read_sql_output(sql_output_file,
+                             location,
+                             assay)
     
-    # write output bips file
-    write_output_bips(bips_input_file,
-                      bips_output_dict,
-                      num_records)
+    # create the bips input file
+    create_bips_input(sql_df,
+                        cancer_type,
+                        assay,
+                        analyte,
+                        match_type,
+                        intent,
+                        bips_input_file)
 
-def get_bioinfdb_query_data(bioinf_db_csv,
-                             delim):
+def read_sql_output(sql_output_file,
+                    location,
+                    assay):
     '''
-    Read in a SQL query output file and return a dictionary with 
-    the fields included in the header + the entries.
-    @param input bioinf_db_csv: SQL query output
-    @param input delim: file delimeter
-    @param return field_dict: list of fields from SQL query
+    Read the sql output file and return a dataframe with the data.
+
+    @param sql_output_file: The file containing the sql output
+    @return sql_df: The dataframe with the sql output
     '''
+    # check if file exists, if not, exit with error
+    try:
+        with open(sql_output_file) as f:
+            pass
+        f.close()
+    except FileNotFoundError:
+        print(f"File {sql_output_file} not found.")
+        exit(1)
 
-    # TODO: make it so that only unique IDs are included
+    # read as a csv file and return the dataframe
+    sql_df = pd.read_csv(sql_output_file)
 
-    field_dict = dict()
+    # rename 'fastq_url' to 'gcs_tumor_fastq_url'
+    sql_df.rename(columns={'fastq_url': 'gcs_tumor_fastq_url'}, inplace=True)
+    # change location in the fastq url from 'prd' to the location provided
+    sql_df['gcs_tumor_fastq_url'] = sql_df['gcs_tumor_fastq_url'].str.replace('prd', location)
 
-    f = open(bioinf_db_csv, 'r')
-    header = f.readline()
-    header_list_ = header.strip().split(delim)
-    header_list = []
+    # replace the assay column with the assay type provided
+    sql_df['assay'] = assay
+
+    return sql_df
+
+def create_bips_input(sql_df,
+                        cancer_type,
+                        assay,
+                        analyte,
+                        match_type,
+                        intent,
+                        bips_input_file):
+        '''
+        Create the bips input file from the sql output.
     
-    # change fastq_url --> gcs_tumor_fastq_url
-    for h in header_list_:
-        if 'fastq_url' in h:
-            header_list.append('gcs_tumor_fastq_url')
-        else: header_list.append(h) 
+        @param sql_df: The dataframe with the sql output
+        @param cancer_type: The cancer type
+        @param assay: The assay type
+        @param analyte: The analyte type
+        @param match_type: The match type
+        @param intent: The intent
+        @param bips_input_file: The file to write the bips input
+
+        '''
+
+        # add the cancer type, assay, match type, and intent to the dataframe
+        sql_df['cancer_type'] = cancer_type
+        sql_df['assay'] = assay
+        sql_df['analyte'] = analyte
+        sql_df['match_type'] = match_type
+        sql_df['intent'] = intent
+
+        # Write the df in order:
+        # orderhub_id,gcs_tumor_fastq_url,cancer_type,assay,analyte,match_type,intent
+        sql_df.to_csv(bips_input_file, index=False, columns=BIPS_FIELDS)
     
-    for line in f:
-        L = line.strip().split(delim)
-        for field_data in L:
-            field_idx = L.index(field_data)
-            field_header = header_list[field_idx]
-            try:
-                field_dict[field_header].append(field_data)
-            except KeyError:
-                field_dict[field_header] = [field_data]
-    f.close()
-    return field_dict
-
-def fill_bips_data(input_file_field_dict,
-                   user_input,
-                   num_records):
-    '''
-    create a dict data structure with keys in the BIPS input
-    from the SQL input file. if the input file does not include some
-    fields, look in the user fields (args)
-    @param input_file_field_dict:
-    @param user_input:
-    @return bips_output_dict: 
-    '''
-    bips_output_dict = dict()
-    for i, bf in enumerate(BIPS_FIELDS):
-        try:
-            # get field data from input file
-            bips_output_dict[bf] = input_file_field_dict[bf]
-        except KeyError:
-            # need to get field data from cli args
-            match bf:
-                case 'orederhub_id':
-                    cli_input = [user_input['ohid']] * num_records   ## TODO: this might be required
-                case 'gcs_tumor_fastq_url':
-                    cli_input = [user_input['fq']] * num_records      ## TODO: this might be required
-                case 'cancer_type':
-                    cli_input = [user_input['cncr']] * num_records
-                case 'assay':
-                    cli_input = [user_input['as']] * num_records
-                case 'analyte':
-                    cli_input = [user_input['an']] * num_records
-                case 'match_type':
-                    cli_input = [user_input['mt']] * num_records
-                case 'intent':
-                    cli_input = [user_input['int']] * num_records
-                case 'control_sample_type':
-                    cli_input = [user_input['cst']] * num_records
-            bips_output_dict[bf] = cli_input
-
-            print('no field in input file for:', bf)
-
-    return bips_output_dict
-
-def write_output_bips(bips_input_file,
-                      bips_output_dict,
-                      num_records): 
-    '''
-    Writes out a file which can be used as input for BIPS.
-    @param bips_input_file: output file path
-    @param bips_output_dict: dictionary containing BIPS data
-    @param num_records: number of records to write
-    '''  
-    # write out to a bips_input file
-    o = open(bips_input_file, 'w')
-    # writing BIPS header
-    for i, bf in enumerate(BIPS_FIELDS):
-        if i == 0:
-            o.write(bf)
-        else:
-            o.write(',' + bf)
-    o.write('\n')
-    
-    # writing a field information for each record
-    for r in range(num_records):
-        record_string = ''
-        for i_, bf_ in enumerate(BIPS_FIELDS):
-            if i_ == 0:
-                record_string += bips_output_dict[bf_][r]
-            else:
-                record_string += ',' + bips_output_dict[bf_][r]
-        o.write(record_string + '\n')
 
 if __name__ == '__main__':
     main()
